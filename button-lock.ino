be10dc970f22e89297769e80d53430878807f076
sventec <simonre@proctoracademy.org>
2017-04-23 19:33:00 -0400

    added button-lock.ino

---
+++ button-lock.ino
@@ -0,0 +1,24 @@
+//Reed Simon
+//4.23.17
+//v1.00
+
+const byte button1 = 3;
+const byte button2 = 4;
+const byte button3 = 5;
+
+const byte led1 = 7;
+const byte led2 = 8;
+
+void setup(){
+    pinMode(button1, INPUT);
+    pinMode(button2, INPUT);
+    pinMode(button3, INPUT);
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+}
+
+void loop(){
+    
+    
+    
+}
